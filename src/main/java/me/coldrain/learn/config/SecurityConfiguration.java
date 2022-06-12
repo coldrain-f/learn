61b8c3a2f9e2ab8d82c99b81f7ff9908fb61848b
coldrain-f <intmain__@naver.com>
2022-06-12 17:29:40 +0900

    init

---
+++ src/main/java/me/coldrain/learn/config/SecurityConfiguration.java
@@ -0,0 +1,20 @@
+package me.coldrain.learn.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity // 부트에서는 없어도 동작한다고 함.
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+}
