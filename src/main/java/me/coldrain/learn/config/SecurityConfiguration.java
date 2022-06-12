e2c0eef11b91bb0f489279bc60f31f1431504171
coldrain-f <intmain__@naver.com>
2022-06-12 17:41:03 +0900

    스프링 시큐리티 기본 설정완료

--- src/main/java/me/coldrain/learn/config/SecurityConfiguration.java
+++ src/main/java/me/coldrain/learn/config/SecurityConfiguration.java
@@ -10,11 +10,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        super.configure(web);
+        web.ignoring()
+                .antMatchers("/resources/**")
+                .antMatchers("/h2-console/**")
+        ;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.csrf().disable()
+                .cors().disable()
+                .authorizeRequests()
+                .antMatchers("/h2-console/**").permitAll()
+                .anyRequest().authenticated()
+        ;
     }
 }
